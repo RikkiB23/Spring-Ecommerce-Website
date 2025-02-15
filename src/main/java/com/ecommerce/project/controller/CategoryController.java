2380341e85491abe695fb594fb59440fbe25f143
RikkiB23 <134138640+RikkiB23@users.noreply.github.com>
2025-02-15 13:34:49 +0000

    Addition of H2 database

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,15 +2,16 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api") //we know that all the mappings will begin with /api so we can add the RequestMapping at the Class level
 public class CategoryController {
 
     //@Autowired --> This would remove the need for the constructor injection, but this is not the approved method
@@ -21,15 +22,38 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/api/public/categories")
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    //@GetMapping("/api/public/categories") this has /api in front, but we have added @RequestMapping at class level so this will be removed everywhere at Method level
+    @GetMapping("/public/categories")
+    //@RequestMapping(value = "/public/categories", method = RequestMethod.GET) //this does the same as the @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> allCategories = categoryService.getAllCategories();
+        return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 
-    @PostMapping("/api/public/categories")
-    public String createCategory(@RequestBody Category category) {
+    @PostMapping("/public/categories")
+    public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
-        return "Category added successfully";
-
+        return new ResponseEntity<>("Category Added Successfully", HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/admin/categories/{categoryID}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryID) { //we're using ResponseEntity to provide the correct status codes
+        try {
+            String status = categoryService.deleteCategory(categoryID);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
+
+    @PutMapping("/public/categories/{categoryID}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryID) {
+        try {
+            Category savedCategory = categoryService.updateCategory(category, categoryID);
+            return new ResponseEntity<>("Category with Category ID: " + categoryID + " updated", HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
+
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -22,5 +24,32 @@ public class CategoryServiceImpl implements CategoryService{
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryID) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryID)) //for every category, we're checking if the category ID matches the ID we want
+                .findFirst() //if it matches, get the first instance of the matching ID and assign it to the Category object
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found")); //this will throw a 404 NOT FOUND exception if the category ID doesn't exist
+
+        categories.remove(category);
+        return "Category with Category ID: " + categoryID + " has been removed.";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryID) {
+        Optional<Category> optionalCategory = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryID))
+                .findFirst();
+        //we do the above because the category might not exist
+
+        if (optionalCategory.isPresent()) {
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        }
     }
 }
